2dd4acfc0917e05be549af5bb119584f6bf288e6
Dominik Szmek <do@minik.pl>
2024-05-11 08:46:00 +0200

    Add func to read contacts from json file

--- signal_send.py
+++ signal_send.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 from re import findall as re_findall
 import requests
@@ -7,10 +8,21 @@ import json
 import uuid
 import argparse
 from datetime import datetime as dt
+import signal
 
 from .settings import URL
 from .emoicons import emoicons
 from .recipients import recipients
+
+
+TIMEOUT = 120
+
+class AlarmException(Exception):
+    pass
+
+
+def alarm_handler(signum, frame):
+    raise AlarmException
 
 
 parser = argparse.ArgumentParser()
@@ -22,6 +34,24 @@ parser.add_argument("-e", "--emoicons", action="store_true",
 
 args = parser.parse_args()
 
+
+with open(f"{os.path.expanduser('~')}/.local/share/signal-send/contacts.json", "r") as f:
+    contacts = iter(json.load(f))
+
+def istartswith(name, pattern):
+    if name is None:
+        return None
+    return name.lower().startswith(pattern)
+
+def find_contact(pattern):
+
+    results = []
+    while result := next(
+            (i
+             for i in contacts
+             if istartswith(i["profile"]["givenName"], pattern)), False):
+        results.append(result)
+    return results
 
 def replace_emoicons(message):
 
@@ -78,18 +108,48 @@ def main():
         print(emoicons)
         sys.exit(0)
 
-    try:
-        recipient = recipients[args.recipient]
-    except KeyError:
-        print("recipient not found.")
+    # try:
+    #     recipient = recipients[args.recipient]
+    # except KeyError:
+    #     print("recipient not found.")
+    #     sys.exit(1)
+
+    recipient = find_contact(args.recipient)
+
+    if len(recipient) == 0:
+        print('no recipient found.')
         sys.exit(1)
+
+    if len(recipient) > 1:
+        print('more than 1 found!')
+        results = [r['profile']['givenName'] for r in recipient]
+        print(results)
+        sys.exit(1)
+
+    recipient = recipient[0]
+    recipient_number = recipient.get("number")
+
+    if 'profile' in recipient:
+        recipient_name = "{} {}".format(
+            recipient["profile"]["givenName"],
+            recipient["profile"]["familyName"]
+        )
 
     message = args.message
 
     if message is None:
-        print(f"Run interactive mode for {args.recipient} {recipient}")
+        print(f"Run interactive mode for {recipient_name} {recipient_number}")
+
         while True:
-            message = input("> ")
+            signal.signal(signal.SIGALRM, alarm_handler)
+            signal.alarm(TIMEOUT)            
+            try:
+                message = input("> ")
+                signal.alarm(0)
+            except AlarmException:
+                print(f'\nInput timeout {TIMEOUT} sec. Quitting...')
+                sys.exit(0)
+
             n = len(message) + 3
 
             # Quit interactive mode
@@ -100,15 +160,15 @@ def main():
             # Protect sending to someone else
             if message.startswith("s "):
                 print("s ??? quitting...")
-                sys.exit(0)
+                sys.exit(1)
                 
-            timestamp, result = send_message(message, recipient)
+            timestamp, result = send_message(message, recipient_number)
             if result[0][1] == 'SUCCESS':
                 print(f"\033[F\33[{n}C\033[0;34;8m  {timestamp}\033[0;0m")
             else:
                 print(f"{timestamp} {result}")
                 
-    timestamp, result = send_message(message, recipient)
+    timestamp, result = send_message(message, recipient_number)
     print(f"{timestamp} {result}")
 
 
