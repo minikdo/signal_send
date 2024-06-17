c4052e2420e78044b110f16a449f9d6d402fd65e
Dominik Szmek <do@minik.pl>
2024-06-17 19:48:16 +0200

    Add remote_delete and clean up code

--- signal_send.py
+++ signal_send.py
@@ -12,10 +12,10 @@ import signal
 
 from .settings import URL
 from .emoicons import emoicons
-#from .recipients import recipients
 
 
 TIMEOUT = 120
+MSG_CHAR_LIMIT = 5000
 
 class AlarmException(Exception):
     pass
@@ -29,6 +29,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("recipient", nargs="?")
 parser.add_argument("message", nargs="?")
 parser.add_argument("-l", "--list", action="store_true", help="list contacts")
+parser.add_argument("-r", "--remove", action="store_true", help="remove contact")
 parser.add_argument("-e", "--emoicons", action="store_true",
                     help="list emoicons")
 
@@ -87,6 +88,7 @@ def send_message(message, recipient):
     response = response.json()
 
     timestamp = response['result']['timestamp'] / 1000.0
+    timestamp_orig = response['result']['timestamp']
     timestamp = dt.fromtimestamp(timestamp).strftime('%H:%M:%S')
 
     result = []
@@ -95,24 +97,72 @@ def send_message(message, recipient):
         status = item['type']
         result.append((number, status))
 
-    return timestamp, result
+    return timestamp, result, timestamp_orig
+
+def remote_delete(recipient, timestamp):
+
+    payload = {
+        "id": str(uuid.uuid1()),
+        "jsonrpc": "2.0",
+        "method": "remoteDelete",
+        "params": {
+            "target-timestamp": timestamp,
+            "recipient": recipient
+        }
+    }
+
+    response = requests.post(URL, json=payload, timeout=10)
+    response = response.json()
+
+    return response
+
+def remove_contact(recipient):
+
+    payload = {
+        "id": str(uuid.uuid1()),
+        "jsonrpc": "2.0",
+        "method": "removeContact",
+        "params": {
+            "identifier": recipient,
+        }
+    }
+
+    response = requests.post(URL, json=payload, timeout=10)
+    response = response.json()
+
+    return response
 
 
 def main():
 
     if args.list:
-        print(json.dumps(recipients, indent=2))
+        from operator import itemgetter
+        from tabulate import tabulate
+        res = []
+        for contact in contacts:
+            name = f"{contact['profile']['givenName'] or '?'} "
+            name += f"{contact['profile']['familyName'] or ''}"
+            res.append([
+                name.lower(),
+                contact['number'],
+                contact['uuid'],
+            ])
+        res.sort(key=lambda e: (e[0] is None, e[0]))
+        print(tabulate(res,
+                       headers=('Name', 'Number', 'UUID'),
+                       showindex=True))
+        sys.exit(0)
+
+
+    if args.remove:
+        result = remove_contact(args.recipient)
+        print(result)
+
         sys.exit(0)
 
     if args.emoicons:
         print(emoicons)
         sys.exit(0)
-
-    # try:
-    #     recipient = recipients[args.recipient]
-    # except KeyError:
-    #     print("recipient not found.")
-    #     sys.exit(1)
 
     recipient = find_contact(args.recipient)
 
@@ -128,6 +178,7 @@ def main():
 
     recipient = recipient[0]
     recipient_number = recipient.get("number")
+    recipient_uuid = recipient.get("uuid")
     recipient_givenname = recipient["profile"]["givenName"]
     recipient_familyname = recipient["profile"]["familyName"]
 
@@ -136,9 +187,17 @@ def main():
 
     message = args.message
 
-    if message is None:
-        print(f"Run interactive mode for {recipient_name} {recipient_number}")
+    # Check pipe
+    if message == '-' and not os.isatty(sys.stdin.fileno()):
+        message = sys.stdin.read()
+        if len(message) > MSG_CHAR_LIMIT:
+            print(f"Input exceedes {MSG_CHAR_LIMIT} characters. Exiting...")
+            sys.exit(1)
 
+    if message is None:
+        print(f"Run interactive mode for {recipient_name}{recipient_number or recipient_uuid}")
+
+        timestamp_orig = 0
         while True:
             signal.signal(signal.SIGALRM, alarm_handler)
             signal.alarm(TIMEOUT)            
@@ -156,18 +215,23 @@ def main():
                 print("quit.")
                 sys.exit(0)
 
+            if message == 'del':
+                result = remote_delete(recipient_uuid, timestamp_orig)
+                print(result['result']['results'])
+                break
+
             # Protect sending to someone else
             if message.startswith("s "):
                 print("s ??? quitting...")
                 sys.exit(1)
                 
-            timestamp, result = send_message(message, recipient_number)
+            timestamp, result, timestamp_orig = send_message(message, recipient_uuid)
             if result[0][1] == 'SUCCESS':
-                print(f"\033[F\33[{n}C\033[0;34;8m  {timestamp}\033[0;0m")
+                print(f"\033[F\33[{n}C\033[0;34;8m  {timestamp} \033[38;5;239m{timestamp_orig}\033[0;0m")
             else:
                 print(f"{timestamp} {result}")
                 
-    timestamp, result = send_message(message, recipient_number)
+    timestamp, result, _ = send_message(message, recipient_number)
     print(f"{timestamp} {result}")
 
 
